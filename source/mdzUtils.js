57bbb7bc5a7fca80474a858472d1855895d4a177
GrumpyGel <70948530+GrumpyGel@users.noreply.github.com>
2021-07-13 11:15:45 +1200

    Update mdzUtils.js

--- source/mdzUtils.js
+++ source/mdzUtils.js
@@ -7,7 +7,6 @@ Coding for Common MyDocz functions.
 
 Functions are defined as static methods on a mdzUtils class as a simple method to stop naming conflicts with other libraries.
 
-
 */
 
 class mdzUtils {
